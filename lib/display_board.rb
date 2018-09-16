8abfcf343597c8cc25eb9796c23b931d97a32f3d
Suheyl Turac <skbsturac@live.com>
2018-09-16 16:56:35 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
+  puts "3x3 Tic Tac Toe Board"
 end
